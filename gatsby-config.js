06160a35890d9345337ea5b80cbd8dfc02e2a642
Peter Okwara <peter.okwara@outlook.com>
2022-08-11 21:58:00 +0300

    Add changes to frontend

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: `personal-website`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://peterokwara.com`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }]
 };
