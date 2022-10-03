8e25e5ba237d559a1904617f0b3add7ef81e3425
P.Okwara <peter.okwara@outlook.com>
2022-10-03 20:28:27 +0300

    Update blog styling

--- src/components/Common/Blog/Blog.js
+++ src/components/Common/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { graphql } from 'gatsby'
-import { Helmet } from "react-helmet";
+import { graphql } from "gatsby";
+import { Header } from "../../Layout/Header/Header";
 
 // import '../css/blog-post.css'; // make it pretty!
 
@@ -9,15 +9,28 @@ export default function Blog({
 }) {
     const { markdownRemark: post } = data; // data.markdownRemark holds your post data
     return (
-        <div className="blog-post-container">
-            <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
-            <div className="blog-post">
-                <h1>{post.frontmatter.title}</h1>
+        <main className="flex flex-col w-full h-screen bg-gray">
+            <Header />
+            <section className="m-8 lg:px-40">
+                <h1 className="text-4xl font-semibold">{post.frontmatter.title}</h1>
                 <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
-            </div>
-        </div>
+            </section>
+        </main>
+        // <div className="blog-post-container">
+        //     <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
+        //     <div className="blog-post">
+        //         <h1>{post.frontmatter.title}</h1>
+        //         <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+        //     </div>
+        // </div>
     );
 }
+
+export const Head = ({ data }) => {
+    const { markdownRemark: post } = data; // data.markdownRemark holds your post data
+
+    return <title>{post.frontmatter.title}</title>;
+};
 
 export const pageQuery = graphql`
     query BlogPostByPath($path: String!) {
