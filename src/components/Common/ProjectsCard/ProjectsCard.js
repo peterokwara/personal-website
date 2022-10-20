8ff89f7cebd1b589028645aab477c71fe85b1032
Peter Okwara <peter.okwara@outlook.com>
2022-10-21 00:32:25 +0300

    Update styling

--- src/components/Common/ProjectsCard/ProjectsCard.js
+++ src/components/Common/ProjectsCard/ProjectsCard.js
@@ -5,12 +5,12 @@ export const ProjectCard = props => {
         <a href={props.url}>
             <div
                 className={
-                    "relative flex flex-col p-8 my-8 rounded-lg bg-card-background h-auto lg:h-96 lg:w-full w-full drop-shadow-md transform hover:scale-105 transition duration-300"
+                    "relative flex flex-col p-8 my-8 rounded-lg bg-card-background h-auto lg:h-80 lg:w-full w-full drop-shadow-md transform hover:scale-105 transition duration-300"
                 }
             >
                 <h5 className="mb-2 text-xl font-semibold text-headline">{props.name}</h5>
                 <h5 className="mb-2 text-base font-normal text-headline">{props.description}</h5>
-                <div className="flex flex-row flex-wrap py-4">{props.children}</div>
+                <div className="flex flex-row flex-wrap py-2">{props.children}</div>
             </div>
         </a>
     );
